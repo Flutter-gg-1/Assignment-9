dcf1c9bd8c09b6a264bd8749e7eab2e569f60809
Amjad Elahi <amjadelahi07@gmail.com>
2024-08-07 21:05:25 +0300

    implements check null function

--- bin/project_details.dart
+++ bin/project_details.dart
@@ -1,23 +1,29 @@
 import 'team.dart';
-class ProjectDetails{
+import 'check_null_function.dart';
+class ProjectDetails {
   final String id;
   final String name;
   final String deadline;
   final List<Team> team;
 
-  ProjectDetails({required this.deadline, required this.id, required this.name, required this.team});
+  ProjectDetails(
+      {required this.deadline,
+      required this.id,
+      required this.name,
+      required this.team});
 
-  factory ProjectDetails.fromJson(Map<String, dynamic> json){
-    return ProjectDetails(deadline: json["deadline"], id: json["id"], name: json["name"], team: List.from(json["team"] as List));
+  factory ProjectDetails.fromJson(Map<String, dynamic> json) {
+    return ProjectDetails(
+        deadline:
+            checkNull(value: json["deadline"].toString(), title: "deadline"),
+        id: checkNull(value: json["id"], title: "id"),
+        name: checkNull(value:  json["name"], title: "name"),
+        team: List.from(json["team"] as List));
   }
 
-  Map<String, dynamic> toJson(){
-    return{
-      "id": id,
-      "name": name,
-      "deadline":deadline ,
-      "team": team
-    };
-    
+  Map<String, dynamic> toJson() {
+    return {"id": id, "name": name, "deadline": deadline, "team": team};
   }
+
 }
+
--- bin/team.dart
+++ bin/team.dart
@@ -1,18 +1,16 @@
-
-class Team{
+import 'dart:ffi';
+import 'check_null_function.dart';
+class Team {
   final String employeeId;
   final String role;
 
   Team({required this.employeeId, required this.role});
 
-  factory Team.fromJson(Map <String, dynamic> json){
-    return Team(employeeId: json["employeeId"], role: json["role"]);
+  factory Team.fromJson(Map<String, dynamic> json) {
+    return Team(employeeId: checkNull(value:  json["employeeId"], title: "employeeId"), role:checkNull(value:  json["role"] ,title: "role"));
   }
 
-  Map <String, dynamic>  toJson(){
-    return{
-      "employeeId": employeeId,
-      "role":  role
-    };
+  Map<String, dynamic> toJson() {
+    return {"employeeId": employeeId, "role": role};
   }
 }
