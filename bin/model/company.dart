492599ea323316fe5d2f5386684e657ddbe0b575
Basel-75 <basel_1422@outlook.com>
2024-08-07 22:49:24 +0300

    Create : divied Json into classes

---
+++ bin/model/company.dart
@@ -0,0 +1,10 @@
+class Company {}
+class Engineering{}
+class Employees{}
+class Contact{}
+class Employeeprojects{}
+class Marketing{}
+class Budget{}
+class Expenses{}
+class Projects{}
+class Team{}
