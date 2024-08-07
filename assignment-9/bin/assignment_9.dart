65d7e38b105e048e0e710e739da9fedd5c41130e
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-07 19:51:37 +0300

    Project Worked!

--- assignment-9/bin/assignment_9.dart
+++ assignment-9/bin/assignment_9.dart
@@ -1,5 +1,4 @@
-import 'package:assignment_9/assignment_9.dart' as assignment_9;
 
-void main(List<String> arguments) {
-  print('Hello world: ${assignment_9.calculate()}!');
+void main() {
+  print('Hello, World!');
 }
--- assignment-9/test/assignment_9_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:assignment_9/assignment_9.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
