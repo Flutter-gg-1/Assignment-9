17481ae9524b667b7af4f5937a2524f2c88b2b7d
muhannad <mohnsalzaher@gmail.com>
2024-08-07 17:24:03 +0300

    class projects  added

---
+++ bin/projects.dart
@@ -0,0 +1,26 @@
+class Projects {
+  final String projectId;
+  final String projectName;
+  final String roleInProject;
+
+  Projects(
+      {required this.projectId,
+      required this.projectName,
+      required this.roleInProject});
+
+  factory Projects.fromJson(Map<String, dynamic> jason) {
+    return Projects(
+        projectId: jason['projectId'],
+        projectName: jason['projectName'],
+        roleInProject: jason['roleInProject']);
+  }
+
+  toJason() {
+    Map<String, dynamic>;
+    return {
+      "projectId": projectId,
+      "projectName": projectName,
+      "roleInProject": roleInProject
+    };
+  }
+}
