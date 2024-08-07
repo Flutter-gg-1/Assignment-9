6596df0766233b001cbd18420a266be9161979c9
muhannad <mohnsalzaher@gmail.com>
2024-08-07 17:16:16 +0300

    class Contact added

---
+++ bin/contact.dart
@@ -0,0 +1,18 @@
+class Contact {
+  final String email;
+  final String phone;
+
+  Contact({required this.email, required this.phone});
+
+  factory Contact.fromJson(Map<String, dynamic> jason) {
+    return Contact(email: jason['email'], phone: jason['phone']);
+  }
+
+  toJason() {
+    Map<String, dynamic>;
+    return {
+      "email": email,
+      "phone": phone,
+    };
+  }
+}
