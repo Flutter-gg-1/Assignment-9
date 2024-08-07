315e69000b21de2c951bb926f20baefd6c7670b6
Basel-75 <basel_1422@outlook.com>
2024-08-07 21:56:19 +0300

    Create : Dart project

---
+++ bin/engineering_employees.dart
@@ -0,0 +1,5 @@
+import 'package:engineering_employees/engineering_employees.dart' as engineering_employees;
+
+void main(List<String> arguments) {
+  print('Hello world: ${engineering_employees.calculate()}!');
+}
---
+++ test/engineering_employees_test.dart
@@ -0,0 +1,8 @@
+import 'package:engineering_employees/engineering_employees.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
