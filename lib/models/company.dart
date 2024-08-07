0b9552a22d52c99a62a35368909627a62c519bc2
Ammarx9 <ammarxx919@gmail.com>
2024-08-07 21:33:50 +0300

    ready the main Models

---
+++ lib/models/company.dart
@@ -0,0 +1,50 @@
+
+
+
+
+
+
+class CompanyModel{
+
+  DepartmentsModel departmentsModel;
+  ProjectsModel projectsModel;
+
+
+
+
+}
+
+
+class DepartmentsModel{
+
+  EngineeringModel engineeringModel;
+
+}
+
+
+class ProjectsModel{
+
+}
+
+
+
+class EngineeringModel{
+
+  EmployeesModel employeesModel;
+
+}
+
+
+class EmployeesModel{
+
+}
+
+
+class ContactModel{
+
+}
+
+
+class BudgetModel{
+  
+}
---
+++ test/asg9_test.dart
@@ -0,0 +1,8 @@
+import 'package:asg9/asg9.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
