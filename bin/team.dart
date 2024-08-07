185691a36d37ba5fb8ec2ec3cfa60d253fc746e1
Amjad Elahi <amjadelahi07@gmail.com>
2024-08-07 20:37:22 +0300

    implements team class

---
+++ bin/team.dart
@@ -0,0 +1,18 @@
+
+class Team{
+  final String employeeId;
+  final String role;
+
+  Team({required this.employeeId, required this.role});
+
+  factory Team.fromJson(Map <String, dynamic> json){
+    return Team(employeeId: json["employeeId"], role: json["role"]);
+  }
+
+  Map <String, dynamic>  toJson(){
+    return{
+      "employeeId": employeeId,
+      "role":  role
+    };
+  }
+}
