c558937cb209b3ad92082e6685c2d9fe6e9e01fd
anas <anas.aljuhani.dev@gmail.com>
2024-08-08 03:56:19 +0300

    creating classes: Marketing => Budget =>Expenses  copied Employee class |

--- lib/allclass.dart
+++ lib/allclass.dart
@@ -10,6 +10,32 @@ class DepartmentsCompany {
 }
 
 //////////Departments Holder
+class Marketing {
+  List<Employees> employeeMarketing;
+  final Budget budget;
+
+  Marketing({required this.employeeMarketing, required this.budget});
+
+  factory Marketing.fromJson(Map<String, dynamic> json) {
+    List<Employees> getEmployeeMap = [];
+
+    for (var val in json["employees"]) {
+      getEmployeeMap.add(Employees.fromJson(val));
+    }
+    return Marketing(
+        employeeMarketing: getEmployeeMap,
+        budget: Budget.fromJson(json['budget']));
+  }
+  Map<String, dynamic> toJson() {
+    Map<String, dynamic> map = {};
+    map["employees"] = employeeMarketing;
+    map["budget"] = budget;
+
+    return map;
+  }
+}
+
+//////////Departments Holder
 class Engineering {
   List<Employees> employee;
   Engineering({required this.employee});
@@ -37,6 +63,45 @@ class Engineering {
 //////////Company Holder
 class ProjectCompany {}
 //////////Company Holder
+
+//////////Marketing Holder
+class Budget {
+  final int total;
+  List<Expenses> expenses;
+
+  Budget({required this.total, required this.expenses});
+
+  factory Budget.fromJson(Map<String, dynamic> json) {
+    return Budget(
+        total: json['total'],
+        expenses: List.from(json["expenses"]).map((element) {
+          return Expenses.fromJson(element);
+        }).toList());
+  }
+  Map<String, dynamic> toJson() {
+    Map<String, dynamic> map = {};
+    map["total"] = total;
+    map["expenses"] = expenses;
+    return map;
+  }
+}
+
+class Expenses {
+  final String item;
+  final int amount;
+  Expenses({required this.amount, required this.item});
+
+  factory Expenses.fromJson(Map<String, dynamic> json) {
+    return Expenses(item: json['item'], amount: json['amount']);
+  }
+  Map<String, dynamic> toJson() {
+    Map<String, dynamic> map = {};
+    map["item"] = item;
+    map["amount"] = amount;
+
+    return map;
+  }
+}
 
 //////////Engineering Holder
 class Employees {
