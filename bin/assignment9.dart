4ac233fcb0c34c0bf1fb2f532d1cd442f076054f
Amjad Elahi <amjadelahi07@gmail.com>
2024-08-08 08:12:01 +0300

    update main function

--- bin/assignment9.dart
+++ bin/assignment9.dart
@@ -1,7 +1,8 @@
 import 'display_marketing_employee.dart';
 import 'display_expenses_budget.dart';
 void main(List<String> arguments) {
+  print("\n___________Display all marketing employee_____________\n");
   print(displayMarketingEmployee());
-  print("--------------------");
+  print("\n___________Display expenses budget of each department_____________\n");
   print(displayExpenseBudget());
 }
