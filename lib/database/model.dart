ae3b33bb0f247481b1855e7ff5567d80dae86a54
MAHA Al-ATAWI <mem880.a@gmail.com>
2024-08-08 09:08:58 +0300

    ✅ Adding functionality

--- lib/database/model.dart
+++ lib/database/model.dart
@@ -12,13 +12,10 @@ class Department {
 }
 
 class Engineering {
-  // Properties
   List<Employee> employees;
 
-  // Constructor
   Engineering({required this.employees});
 
-  // Methods
   factory Engineering.fromJson(Map<String, dynamic> json) => Engineering(
       employees: List<Employee>.from(
           json['employees'].map((employee) => Employee.fromJson(employee))));
@@ -26,35 +23,29 @@ class Engineering {
       {'employees': employees.map((employee) => employee.toJson()).toList()};
 }
 
-// ذهب الكثير وبقي الكثير
 class Marketing {
-  // Properties
   List<Employee> employees;
   Budget budget;
 
-  // Constructor
   Marketing({required this.employees, required this.budget});
 
-  // Methods
   factory Marketing.fromJson(Map<String, dynamic> json) => Marketing(
-      budget: Budget.fromJson(json['budget']),
       employees: List<Employee>.from(
-          json['employees'].map((employee) => Employee.fromJson(employee))));
+          json['employees'].map((employee) => Employee.fromJson(employee))),
+      budget: Budget.fromJson(json['budget']));
   Map<String, dynamic> toJson() => {
+        'employees': employees.map((employee) => employee.toJson()).toList(),
         'budget': budget.toJson(),
-        'employees': employees.map((employee) => employee.toJson()).toList()
       };
 }
 
 class Employee {
-  // Properties
   String id;
   String name;
   String role;
   Contact contact;
   List<Project> projects;
 
-  // Constructor
   Employee(
       {required this.id,
       required this.name,
@@ -62,14 +53,15 @@ class Employee {
       required this.contact,
       required this.projects});
 
-  // Methods
   factory Employee.fromJson(Map<String, dynamic> json) => Employee(
       id: json['id'],
       name: json['name'],
       role: json['role'],
       contact: Contact.fromJson(json['contact']),
-      projects: List<Project>.from(
-          json['projects'].map((project) => Project.fromJson(project))));
+      projects: json['projects'] != null
+          ? List<Project>.from(
+              json['projects'].map((project) => Project.fromJson(project)))
+          : []);
   Map<String, dynamic> toJson() => {
         'id': id,
         'name': name,
@@ -80,32 +72,26 @@ class Employee {
 }
 
 class Contact {
-  // Properties
   String email;
   String phone;
 
-  // Constructor
   Contact({required this.email, required this.phone});
 
-  // Methods
   factory Contact.fromJson(Map<String, dynamic> json) =>
       Contact(email: json['email'], phone: json['phone']);
   Map<String, dynamic> toJson() => {'email': email, 'phone': phone};
 }
 
 class Project {
-  // Properties
   String projectId;
   String projectName;
   String roleInProject;
 
-  // Constructor
   Project(
       {required this.projectId,
       required this.projectName,
       required this.roleInProject});
 
-  // Methods
   factory Project.fromJson(Map<String, dynamic> json) => Project(
       projectId: json['projectId'],
       projectName: json['projectName'],
@@ -159,14 +145,14 @@ class ProjectABC {
       required this.team});
 
   factory ProjectABC.fromJson(Map<String, dynamic> json) => ProjectABC(
-      deadline: json['deadline'],
-      name: json['name'],
       id: json['id'],
+      name: json['name'],
+      deadline: json['deadline'],
       team: List<Team>.from(json['team'].map((team) => Team.fromJson(team))));
   Map<String, dynamic> toJson() => {
-        'deadline': deadline,
-        'name': name,
         'id': id,
+        'name': name,
+        'deadline': deadline,
         'team': team.map((team) => team.toJson()).toList()
       };
 }
@@ -180,4 +166,137 @@ class Team {
   factory Team.fromJson(Map<String, dynamic> json) =>
       Team(employeeId: json['employeeId'], role: json['role']);
   Map<String, dynamic> toJson() => {'employeeId': employeeId, 'role': role};
+}
+
+class Departments {
+  Engineering engineering;
+  Marketing marketing;
+
+  Departments({required this.engineering, required this.marketing});
+
+  factory Departments.fromJson(Map<String, dynamic> json) => Departments(
+        engineering: Engineering.fromJson(json['engineering']),
+        marketing: Marketing.fromJson(json['marketing']),
+      );
+
+  Map<String, dynamic> toJson() => {
+        'engineering': engineering.toJson(),
+        'marketing': marketing.toJson(),
+      };
+}
+
+Map<String, dynamic> getMarketingEmployees(Map<String, dynamic> companyData) {
+  try {
+    final marketingDepartment = companyData['departments']['marketing'];
+    if (marketingDepartment == null) {
+      throw FormatException('Marketing department data is missing');
+    }
+
+    final marketingEmployees = marketingDepartment['employees'];
+    if (marketingEmployees == null) {
+      throw FormatException('Marketing employees data is missing');
+    }
+
+    return {'employees': marketingEmployees};
+  } catch (e) {
+    throw FormatException('Error retrieving marketing employees: $e');
+  }
+}
+
+Map<String, dynamic> getEngineeringEmployees(Map<String, dynamic> companyData) {
+  try {
+    final engineeringDepartment = companyData['departments']['engineering'];
+    if (engineeringDepartment == null) {
+      throw FormatException('Engineering department data is missing');
+    }
+
+    final engineeringEmployees = engineeringDepartment['employees'];
+    if (engineeringEmployees == null) {
+      throw FormatException('Engineering employees data is missing');
+    }
+
+    return {'employees': engineeringEmployees};
+  } catch (e) {
+    throw FormatException('Error retrieving engineering employees: $e');
+  }
+}
+
+Map<String, dynamic> getEmployeeProjects(Map<String, dynamic> companyData) {
+  try {
+    final employees = [];
+    final departments = companyData['departments'];
+    if (departments != null) {
+      final engineeringEmployees = departments['engineering']['employees'];
+      final marketingEmployees = departments['marketing']['employees'];
+      if (engineeringEmployees != null) {
+        employees.addAll(engineeringEmployees);
+      }
+      if (marketingEmployees != null) {
+        employees.addAll(marketingEmployees);
+      }
+    }
+
+    return {
+      'projects': employees.map((employee) {
+        return {'id': employee['id'], 'projects': employee['projects'] ?? []};
+      }).toList()
+    };
+  } catch (e) {
+    throw FormatException('Error retrieving employee projects: $e');
+  }
+}
+
+Map<String, dynamic> getBudgetExpenses(Map<String, dynamic> companyData) {
+  try {
+    final departments = companyData['departments'];
+    if (departments == null) {
+      throw FormatException('Departments data is missing');
+    }
+
+    final engineeringBudget = departments['engineering']['budget'];
+    final marketingBudget = departments['marketing']['budget'];
+
+    final expenses = {};
+    if (engineeringBudget != null) {
+      expenses['engineering'] = engineeringBudget['expenses'] ?? [];
+    }
+    if (marketingBudget != null) {
+      expenses['marketing'] = marketingBudget['expenses'] ?? [];
+    }
+
+    return {'expenses': expenses};
+  } catch (e) {
+    throw FormatException('Error retrieving budget expenses: $e');
+  }
+}
+
+void checkNull(Map<String, dynamic> companyData) {
+  try {
+    if (companyData['departments'] == null) {
+      throw FormatException('Departments data is missing');
+    }
+
+    final departments = companyData['departments'];
+    if (departments['engineering'] == null ||
+        departments['marketing'] == null) {
+      throw FormatException('Engineering or Marketing data is missing');
+    }
+
+    final engineeringEmployees = departments['engineering']['employees'];
+    final marketingEmployees = departments['marketing']['employees'];
+    if (engineeringEmployees == null || marketingEmployees == null) {
+      throw FormatException(
+          'Employees data is missing in one or more departments');
+    }
+
+    final marketingBudget = departments['marketing']['budget'];
+    if (marketingBudget != null) {
+      final expenses = marketingBudget['expenses'];
+      if (expenses == null) {
+        throw FormatException('Expenses data is missing in Marketing budget');
+      }
+    }
+  } catch (e) {
+    throw FormatException('Data validation error: $e');
+  }
 }
